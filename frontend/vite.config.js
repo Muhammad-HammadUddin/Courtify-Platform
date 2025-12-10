89c28096cc744ffebe56958368fe86e62766f811
Muhammad Hammad Uddin <hammaduddin083@gmail.com>
2025-12-10 14:13:35 +0500

    fixed vercel.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,5 +10,8 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
-    base: '/'
+    base: '/',
+    build: {
+        chunkSizeWarningLimit: 2000
+    }
 })
